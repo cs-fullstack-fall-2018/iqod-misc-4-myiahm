6024b315acc2aed2eeb565600b9b4f0ed1ada6ef
Christopher Pace <Christopher_pace@ymail.com>
2018-11-12 14:22:02 -0600

    check in

---
+++ answer.js
@@ -0,0 +1,12 @@
+function Array ()
+{
+    var kennIsAwesome = ([["test", "this", "function"], ["another", "test", "ofFunction"], ["this", "is not", "the test"]]);
+     var array1 = kennIsAwesome[1];
+     var array2 = kennIsAwesome[2];
+     var array3 = kennIsAwesome[3];
+    console.log("Array1 \n"+array1+"\n "+"Array2\n"  +array2+"\n " +"Array3\n "+array3);
+}
+
+
+
+Array();
